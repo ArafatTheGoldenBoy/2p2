8133cae0a37059257416560321d6153befe5b373
Yasin Arafat <mdyasinarafat3@gmail.com>
2019-01-18 21:26:49 +0600

    icon fixed

--- 2p2website/js/custom.js
+++ 2p2website/js/custom.js
@@ -5,22 +5,3 @@ jQuery(document).ready(function( $ ) {
         time: 1000
     });
 });
-
-// -- Scrolling Effect
-$(document).ready(function() {
-    $(".menu-icon").on("click", function() {
-          $("nav ul").toggleClass("showing");
-    });
-});
-
-// Scrolling Effect
-
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('black');
-    }
-
-    else {
-          $('nav').removeClass('black');
-    }
-})
